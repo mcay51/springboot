6e110587c37c2bdb2dd40cc0d2f5ff84f76eb153
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-20 23:38:15 +0300

    first commit

---
+++ springboot-moduler-learning/src/main/java/tr/com/mcay/springbootmodulerlearning/SpringbootModulerLearningApplication.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.springbootmodulerlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootModulerLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootModulerLearningApplication.class, args);
+    }
+
+}
---
+++ springboot-moduler-learning/src/test/java/tr/com/mcay/springbootmodulerlearning/SpringbootModulerLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.springbootmodulerlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootModulerLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
