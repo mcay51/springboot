21294f882cd13d95e585b4f2f36ce3e38c5f6c85
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-23 20:48:19 +0300

    interceptor oluşturuldu

---
+++ springboot-moduler-learning/src/main/java/tr/com/mcay/springbootmodulerlearning/interceptors/LogInterceptor.java
@@ -0,0 +1,28 @@
+package tr.com.mcay.springbootmodulerlearning.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Interceptor: Incoming request to " + request.getRequestURI());
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) {
+        System.out.println("Interceptor: post handle response status to " + response.getStatus());
+
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("Interceptor: after complation method " );
+        System.out.println("Interceptor: Incoming request to " + request.getRequestURI());
+        System.out.println("Interceptor: post handle response status to " + response.getStatus());
+    }
+}
